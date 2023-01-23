60624c68d4fea8d1c1225aa02444c4ad7397aa8d
Vdarak <88142861+Vdarak@users.noreply.github.com>
2023-01-23 14:06:18 +0530

    Add files via upload

--- EMAIL SEARCH.py
+++ EMAIL SEARCH.py
@@ -1,5 +1,9 @@
 import imaplib
 import email
+# I used the word weekly instead of congratulations.
+# However when you run this code you can change the word to your desired choice.
+# you can use your own gmail id & password (line 29)
+# if an error comes due to 2 factor authentication login accounts then you might need to use an app password which can be generated in gmail settings
 
 def get_weekly_mails(username, password):
     # Connect to the email server
@@ -7,20 +11,20 @@ def get_weekly_mails(username, password):
     mail.login(username, password)
     mail.select("inbox")
 
-    # Search for recent congratulations emails
+    # Searching for last 10 emails
     result, data = mail.search(None, "ALL")
     ids = data[0].split()
     ids = ids[-10:]
 
-    # Fetch the subject of the emails
+    # Fetching the subject of the emails
     for i in ids:
         result, data = mail.fetch(i, "(RFC822)")
         for response in data:
             if isinstance(response, tuple):
                 msg = email.message_from_bytes(response[1])
                 subject = msg["Subject"]
-                if "security" in subject.lower():
+                if "weekly" in subject.lower():
                     print(subject)
                 else : print ("given word not found in subject")
 
-get_weekly_mails("2017.vedant.darak@ves.ac.in", "qhcnkdsehdcdzlrq")
+get_weekly_mails("username", "password")
